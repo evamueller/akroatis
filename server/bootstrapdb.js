92741bab675abd7265e402109190785568f38e8e
unknown <s3nnyo@googlemail.com>
2016-07-17 00:58:49 +0200

    new story in db

--- server/bootstrapdb.js
+++ server/bootstrapdb.js
@@ -30,8 +30,36 @@ var eva_story = new Story({
     'chapters': []
 });
 
+
+var secondStory_first_chapter = new Chapter({
+    'lat': '51.494683',
+    'long': '7.419428',
+    'name': 'firstChapter',
+    'audio': '',
+    'chapterNumber': 1
+});
+
+var secondStory_second_chapter = new Chapter({
+    'lat': '51.495658',
+    'long': '7.418816',
+    'name': 'firstChapter',
+    'audio': '',
+    'chapterNumber': 2
+});
+
+var second_story = new Story({
+    'author': 'Moritz',
+    'date': Date.now(),
+    'name': 'secondStory',
+    'chapters': []
+});
+
+
 eva_story.chapters.push(eva_first_chapter);
 eva_story.chapters.push(eva_second_chapter);
+
+second_story.chapters.push(secondStory_first_chapter);
+second_story.chapters.push(secondStory_second_chapter);
 
 eva_first_chapter.save(function (err) {
     if (err) {
@@ -52,5 +80,28 @@ eva_story.save(function (err) {
         console.log(err);
     } else {
         console.log('meow');
+    }
+});
+
+
+secondStory_first_chapter.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('wuff');
+    }
+});
+secondStory_second_chapter.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('wuff');
+    }
+});
+secondStory.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('wuff');
     }
 });
