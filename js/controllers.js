6de9be9b59baa8af0911b612dfc847ca7686c4f9
Eva Müller <eva@softwaretechnik-mueller.de>
2016-07-16 14:17:49 +0200

    View

---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('mapCtrl', function($scope) {
+
+})
+   
+.controller('settingsPageCtrl', function($scope) {
+
+})
+         
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('profilCtrl', function($scope) {
+
+})
+   
+.controller('storiesCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+ 
